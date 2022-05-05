79a932234c7f256079abfbddf9f6e626da383682
freek linssen <freeklinssen@MacBook-Pro-van-freek.local>
2022-05-05 18:22:53 +0200

    with norrmalization

--- extractor.py
+++ extractor.py
@@ -4,12 +4,30 @@ from skimage.measure import ransac
 import skimage
 
 
+def add_ones(x):
+  result = np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+  print(result)
+  return result
 
 class extractor(object):
-  def __init__(self):
+  def __init__(self, k):
     self.orb = cv2.ORB_create()
     self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     self.last = None
+    self.k = k
+    self.Kinv = np.linalg.inv(self.k)
+
+  def normalize(self, pt):
+    return(np.dot(self.Kinv, add_ones(pt).T).T[:,0:2])
+
+  def denormalize(self, pt):
+    print(np.array([pt[0], pt[1], 1]))
+    ret = np.dot(self.k, np.array([pt[0], pt[1], 1]).T)
+
+    return int(round(ret[0])), int(round(ret[1]))
+
+    #return int(round(pt[0])+self.w), int(round(pt[1])+self.h)
+
   def extract(self, img):
     feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel= 0.01, minDistance=3)
 
@@ -31,12 +49,26 @@ class extractor(object):
     # filter
     if len(ret) > 0:
       ret = np.array(ret)
+
+
+      print(".")
+      print()
+      print(ret[:, 1, :])
+
+      self
+      ret[:, 0, :] = self.normalize(ret[:,0,:])
+      ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
+
+
       model, inliers = ransac((ret[:, 0], ret[:,1]),
                               skimage.transform.FundamentalMatrixTransform,
                               min_samples=8,
                               residual_threshold=1,
                               max_trials= 100)
       ret = ret[inliers]
+
+      #print(img.shape)
 
     self.last = {"kps": kps, "des": des}
 
--- line_detection.py
+++ line_detection.py
@@ -1,15 +1,20 @@
 import cv2
-from line_detection_george.display import display
+from display import display
 import numpy as np
 from extractor import extractor
 
 w = 1920//2
 h = 1080//2
+
+F = 1
+k = np.array([[F, 0, w//2], [0, F, h//2], [0, 0, F]])
+print(k)
 display = display(w,h)
 orb = cv2.ORB_create()
 
 
-fe = extractor()
+fe = extractor(k)
+
 def process_frame(img):
   img = cv2.resize(img,(w, h))
   # print(img.shape)
@@ -17,9 +22,11 @@ def process_frame(img):
   matches = fe.extract(img)
   print(len(matches))
 
+
+
   for pt1, pt2 in matches:
-    u1,v1 = map(lambda x: int(round(x)), pt1)
-    u2, v2 = map(lambda x: int(round(x)), pt2)
+    u1,v1 = fe.denormalize(pt1)
+    u2, v2 = fe.denormalize(pt2)
     cv2.circle(img,(u1,v1), color = (0,255,0), radius=3)
     cv2.circle(img, (u2, v2), color=(0, 0, 255), radius=3)
     cv2.line(img, (u1,v1), (u2, v2), color=(255, 0, 0))
@@ -33,7 +40,7 @@ def process_frame(img):
 
 
 if __name__ == "__main__":
-  cap = cv2.VideoCapture("line_detection_george/test_countryroad.mp4")
+  cap = cv2.VideoCapture("SLAM_George_Hotz/test_countryroad.mp4")
   while cap.isOpened():
     ret, frame = cap.read()
     if ret == True:
