e8d21a987350ecef37b1f3d788e4242aa730ca97
freek linssen <freeklinssen@MacBook-Pro-van-freek.local>
2022-05-07 15:55:51 +0200

    the point class no also keeps indicies

---
+++ Frame.py
@@ -0,0 +1,117 @@
+import cv2
+import numpy as np
+from skimage.measure import ransac
+import skimage
+
+
+def add_ones(x):
+  result = np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+  return result
+
+IRt = np.eye(4)
+
+
+def extractRt(E):
+  W = np.mat([[0,-1,0],[1,0,0],[0,0,1]], dtype=float)
+  U,d,Vt = np.linalg.svd(E)
+  print(U)
+  print(d)
+  print(Vt)
+  assert np.linalg.det(U) > 0
+  if np.linalg.det(Vt) > 0:
+    Vt *= -1.0
+  R = np.dot(np.dot(U, W), Vt)
+  print(R)
+  if np.sum(R.diagonal()) < 0:
+    R = np.dot(np.dot(U, W.T), Vt)
+  t = U[:, 2]
+  ret = np.eye(4)
+  ret[:3, :3] = R
+  ret[:3, 3] = t
+
+  #Rt = np.concatenate([R, t.reshape(3,1)], axis = 1)
+  return ret
+
+
+def extract(img):
+  orb = cv2.ORB_create()
+  pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel= 0.01, minDistance=3)
+  #extraction retrun points and des
+  kps = [cv2.KeyPoint(x= f[0][0], y=f[0][1], _size=20) for f in pts]
+  kps, des = orb.compute(img, kps)
+
+  return np.array([(kp.pt[0], kp .pt[1]) for kp in kps]), des
+
+
+
+def normalize(Kinv, pt):
+  return np.dot(Kinv, add_ones(pt).T).T[:, 0:2]
+
+def denormalize(k, pt):
+  ret = np.dot(k, np.array([pt[0], pt[1], 1.0]).T)
+  return int(round(ret[0])), int(round(ret[1]))
+
+def match_frames(f1, f2):
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+  ret = []
+  idx1, idx2 = [],[]
+  matches = bf.knnMatch(f1.des, f2.des, k=2)
+
+  for m, n in matches:
+    if m.distance < 0.70 * n.distance:
+      p1 = f1.pts[m.queryIdx]
+      p2 = f2.pts[m.trainIdx]
+      ret.append((p1,  p2))
+      idx1.append(m.queryIdx)
+      idx2.append(m.trainIdx)
+
+  assert len(ret) > 8
+  ret= np.array(ret)
+  idx1 = np.array((idx1))
+  idx2 = np.array((idx2))
+
+
+  model, inliers = ransac((ret[:, 0], ret[:,1]),
+                              #skimage.transform.FundamentalMatrixTransform,
+                              skimage.transform.EssentialMatrixTransform,
+                              min_samples=8,
+                              residual_threshold=0.005,
+                              max_trials=200)
+
+  pts = ret[inliers]
+  print(model.params)
+  Rt = extractRt(model.params)
+
+  return  Rt, idx1[inliers], idx2[inliers]
+
+
+class Frame( ):
+  def __init__(self, img, k):
+    self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    self.k = k
+    self.kinv = np.linalg.inv(self.k)
+
+    pts, self.des = extract(img)
+    self.pose = IRt
+    self.pts = normalize(self.kinv, pts)
+
+
+    # matching
+
+"""
+    if self.last is not None:
+
+
+
+    # filter
+    Rt = None
+    if len(ret) > 0:
+      ret = np.array(ret)
+
+      print(".,")
+      print(ret.shape)
+"""
+
+
+
+
--- line_detection.py
+++ line_detection.py
@@ -1,7 +1,8 @@
 import cv2
 from display import display
 import numpy as np
-from extractor import extractor
+from Frame import Frame, denormalize, match_frames, IRt
+#import g2o
 
 w = 1920//2
 h = 1080//2
@@ -13,23 +14,68 @@ display = display(w,h)
 orb = cv2.ORB_create()
 
 
-fe = extractor(k)
+class Point(object):
+  # point is a 3-D point in the world
+  # each point is observed in muliple frames
 
+
+  def __init__(self, location):
+    self.location = location
+    self.frames = []
+    self.index = []
+
+
+  def add_observation(self, frame, index):
+    self.frames.append(frame)
+    self.index.append(index)
+
+
+frames =[]
 def process_frame(img):
   img = cv2.resize(img,(w, h))
-  # print(img.shape)
-  print("okay")
-  matches, Rt = fe.extract(img)
+  # print(img.shape)]
+  frame = Frame(img, k)
+  frames.append(frame)
+  if len(frames) <= 1:
+    return
 
-  print("Matches", len(matches))
+  Rt, idx1, idx2 = match_frames(frames[-1], frames[-2])
+  #print(pts)
+  print(Rt)
+  print(IRt)
 
-  #if Rt is None:
-   # return
+  # triangulate
+  pts4d = cv2.triangulatePoints(IRt[:3], Rt[:3], frames[-1].pts[idx1].T, frames[-2].pts[idx2].T).T
+
+  # homogenious 3d coordinates (just make the last coordinate zero)
+  pts4d /= pts4d[:, 3:]
+  print(pts4d)
+
+  frames[-1].pose = np.dot(Rt, frames[-2].pose)
+
+  #rint(frames[-1].pose)
+
+  #reject points without enough "paralax" and reject poinit behind the camera
+  good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
 
 
-  for pt1, pt2 in matches:
-    u1,v1 = fe.denormalize(pt1)
-    u2, v2 = fe.denormalize(pt2)
+  for i, location in enumerate(pts4d):
+    if not good_pts4d[i]:
+      continue
+    pt = Point(location)
+    pt.add_observation(frames[-1], idx1[i])
+    pt.add_observation(frames[-2], idx2[i])
+
+  print(sum(pts4d), len(pts4d))
+
+
+
+
+  print("Matches", len(frames[-1].pts))
+
+  for pt1, pt2 in zip(frames[-1].pts[idx1], frames[-2].pts[idx2]):
+    u1,v1 = denormalize(k, pt1)
+    u2, v2 = denormalize(k, pt2)
     cv2.circle(img,(u1,v1), color = (0,255,0), radius=3)
     cv2.circle(img, (u2, v2), color=(0, 0, 255), radius=3)
     cv2.line(img, (u1,v1), (u2, v2), color=(255, 0, 0))
